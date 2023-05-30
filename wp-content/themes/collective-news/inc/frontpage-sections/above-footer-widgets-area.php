a8c58dd992f6113591764056b92893a2ee90a531
trinidadmcdavid <trinidadmcdavid482@gmail.com>
2023-05-30 15:55:55 +0600

    ww.docs.stord.com-upload

--- wp-content/themes/collective-news/inc/frontpage-sections/above-footer-widgets-area.php
+++
@@ -1,9 +0,0 @@
-<?php if ( is_active_sidebar( 'above-footer-widgets-area' ) ) : ?>
-	<div class="above-footer-widget-section">
-		<div class="theme-wrapper">
-			<div class="above-footer-widget-section-wrap">
-				<?php dynamic_sidebar( 'above-footer-widgets-area' ); ?>
-			</div>
-		</div>
-	</div>
-<?php endif; ?>
